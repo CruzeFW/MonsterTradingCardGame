221ee6df89cc9ac69f59e53d7d8dc9866bbcc41c
CruzeFW <128750017+CruzeFW@users.noreply.github.com>
2023-12-21 16:37:04 +0100

    updated mtcg_app.sql with ON DELETE and correct values

--- sql/mtcg_app.sql
+++ sql/mtcg_app.sql
@@ -2,12 +2,14 @@ CREATE DATABASE mtcg_db;
 /*
     DROP DATABASE mtcg_db;
     DROP TABLE users;
-
-
+    DROP TABLE packages;
+    DROP TABLE cards;
+    DROP TABLE battles;
+    DROP TABLE transactions;
 */
 
 CREATE TABLE IF NOT EXISTS users (
-    uuid VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     bio VARCHAR(255),
@@ -16,45 +18,53 @@ CREATE TABLE IF NOT EXISTS users (
     coins INT NOT NULL
     );
 
+CREATE TABLE IF NOT EXISTS packages (
+    id VARCHAR(255) PRIMARY KEY,
+    bought boolean NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS cards (
-    carduid VARCHAR(255) PRIMARY KEY,
-    cardname VARCHAR(255) NOT NULL,
+    id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
     damage FLOAT NOT NULL,
     owner VARCHAR(255),
     packageid VARCHAR(255),
     CONSTRAINT fk_packages
         FOREIGN KEY (packageid)
-        REFERENCES packages(packageuid)
-);
-
-CREATE TABLE IF NOT EXISTS packages (
-    packageuid VARCHAR(255) PRIMARY KEY,
-    bought boolean NOT NULL
+        REFERENCES packages(id)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS battles (
-    battleuid VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     user1 VARCHAR(255) NOT NULL,
     user2 VARCHAR(255),
     winner VARCHAR(255),
     loser VARCHAR(255),
     log VARCHAR(255),
-    CONSTRAINT fk_user
-        FOREIGN KEY (user1, user2)
-        REFERENCES users(uuid)
+    CONSTRAINT fk_user1
+        FOREIGN KEY (user1)
+        REFERENCES users(id)
+        ON DELETE CASCADE,
+    CONSTRAINT fk_user2
+        FOREIGN KEY (user1)
+        REFERENCES users(id)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS transactions (
-    transactionid VARCHAR(255) PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     buyer VARCHAR(255) NOT NULL,
     packageid VARCHAR(255) NOT NULL,
     completed boolean NOT NULL,
     CONSTRAINT fk_user
         FOREIGN KEY (buyer)
-        REFERENCES users(uuid),
+        REFERENCES users(id)
+        ON DELETE CASCADE,
     CONSTRAINT fk_package
         FOREIGN KEY (packageid)
-        REFERENCES packages(packageuid)
+        REFERENCES packages(id)
+        ON DELETE CASCADE
 );
 
 
