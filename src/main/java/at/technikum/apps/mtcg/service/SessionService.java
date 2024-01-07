271b4736516b7955c4324fe83809acba235038cd
CruzeFW <128750017+CruzeFW@users.noreply.github.com>
2024-01-07 19:36:35 +0100

    reworked session controller/service, now in unison with the other controllers

--- src/main/java/at/technikum/apps/mtcg/service/SessionService.java
+++ src/main/java/at/technikum/apps/mtcg/service/SessionService.java
@@ -3,14 +3,38 @@ package at.technikum.apps.mtcg.service;
 import at.technikum.apps.mtcg.dto.TokenRequest;
 import at.technikum.apps.mtcg.entity.Token;
 import at.technikum.apps.mtcg.repository.TokenRepositoryDatabase;
+import at.technikum.server.http.Request;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 public class SessionService {
 
     private final TokenRepositoryDatabase tokenRepositoryDatabase;
+    private final Token token;
 
     public SessionService(TokenRepositoryDatabase tokenRepositoryDatabase) {
         this.tokenRepositoryDatabase = tokenRepositoryDatabase;
+        this.token = new Token();
+    }
+
+    public Integer postMethodCalled(Request request) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        TokenRequest tokenRequest = null;
+        try {
+            tokenRequest = objectMapper.readValue(request.getBody(), TokenRequest.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        token.setExists(validateToken(tokenRequest));
+
+        if (token.getExists()) {
+            token.setToken(tokenRequest.getUsername() + "-mtcgToken");
+            addTokenToUser(token, tokenRequest);
+            return 0;
+        }
+        return 1;
     }
 
     public boolean validateToken(TokenRequest tokenRequest){
@@ -20,6 +44,7 @@ public class SessionService {
     public void addTokenToUser(Token token, TokenRequest tokenRequest){
         tokenRepositoryDatabase.addToken(token, tokenRequest);
     }
+
 
 
 }
